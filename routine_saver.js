14159236acce181018cfb5009fe7b2f4ce417959
rhit-hausmabq <hausmabq@rose-hulman.edu>
2026-09-25 17:10:04 -0400

    real new fix hopefully

--- routine_saver.js
+++ routine_saver.js
@@ -84,12 +84,13 @@ enter_button.addEventListener("click", function(){
 let save = document.getElementById("save");
 save.addEventListener("click", function(){
     let message = document.getElementById("save-message");
-    message.innerHTML = "Saving2...";
+    message.innerHTML = "Saving3...";
     localStorage.clear();
+
     let saveDataString = "";
     let tableRows = document.getElementById("table").children;
     for(row of tableRows) {
-        saveDataString = saveDataString + row[0].value + " " + row[1].value + ", ";
+        saveDataString = saveDataString + row[0].textContent + " " + row[1].textContent + ", ";
     }
     localStorage.setItem("scheduleData", saveDataString);
     message.innerHTML = "Schedule data successfully saved!";
