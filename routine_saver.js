971d53f5c89286e3b7fc43006d065843aaba453f
rhit-hausmabq <hausmabq@rose-hulman.edu>
2026-09-25 18:20:01 -0400

    change again

--- routine_saver.js
+++ routine_saver.js
@@ -89,7 +89,7 @@ save.addEventListener("click", function(){
     let saveDataString = "";
     let tableRows = document.getElementById("table").children;
     for(let row of tableRows) {
-        saveDataString = saveDataString + row.cells[0].innerHTML + " " + row.cells[1].innerHTML + ", ";
+        saveDataString = saveDataString + row.textContent + " " + row.textContent + ", ";
     }
     localStorage.setItem("scheduleData", saveDataString);
     let test = document.getElementById("test");
