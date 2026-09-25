a65e991e6fbeb3019605dd82195cd0261a0726e0
rhit-hausmabq <hausmabq@rose-hulman.edu>
2026-09-25 18:44:39 -0400

    Fixing bugs I hope

--- routine_saver.js
+++ routine_saver.js
@@ -87,7 +87,7 @@ save.addEventListener("click", function(){
     let saveDataString = "";
     let tableRows = document.getElementById("table").children;
     for(let row of tableRows) {
-        saveDataString = saveDataString + row.children[0].textContent + " " + row.children[1].textContent + ", ";
+        saveDataString = saveDataString + row.children[0].textContent + "-" + row.children[1].textContent + ", ";
     }
     localStorage.setItem("scheduleData", saveDataString);
     let test = document.getElementById("test");
@@ -103,7 +103,7 @@ window.addEventListener("DOMContentLoaded", function(){
     let rowValues = localStorage.getItem("scheduleData").split(", ");
     let table = document.getElementById("table");
     for(let value of rowValues){
-        let rowEntries = value.split(" ");
+        let rowEntries = value.split("-");
         let time = document.createElement("td");
         let inputData = document.createElement("td");
         let input = document.createElement("input");
